f0d91d2c9a4f38161c19c1614a94f17370535dc3
ambuj vashistha <ambujva123@gmail.com>
2026-04-07 12:59:55 +0530

    added  MSE visualization

--- app.py
+++ app.py
@@ -12,13 +12,22 @@ y = 2 * X + 3 + np.random.randn(50)
 
 m = st.slider("Slope (m)", -5.0, 5.0, 0.0, 0.1)
 b = st.slider("Intercept (b)", -10.0, 10.0, 0.0, 0.1)
+
 y_pred = m * X + b
+residuals = y - y_pred
 
 fig, ax = plt.subplots()
 
 ax.scatter(X, y, label="Data")
 ax.plot(X, y_pred, color="red", label="Model")
 
+# residual lines
+for i in range(len(X)):
+    ax.plot([X[i], X[i]], [y[i], y_pred[i]], color="gray", alpha=0.5)
+
 ax.legend()
 
 st.pyplot(fig)
+
+mse = np.mean(residuals ** 2)
+st.write("MSE:", round(mse, 3))
